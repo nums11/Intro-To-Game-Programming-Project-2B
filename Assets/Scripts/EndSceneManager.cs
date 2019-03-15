8cc0ab99cc3332c2c4b15ae905709d7733d41ad3
Numfor Tiapo <numsmt2@gmail.com>
2019-03-15 14:44:17 -0400

    Adding Music and Sound Effects

--- Assets/Scripts/EndSceneManager.cs
+++ Assets/Scripts/EndSceneManager.cs
@@ -6,28 +6,34 @@ using UnityEngine.UI;
 public class EndSceneManager : MonoBehaviour
 {
     Button mainMenuBtn, playAgainBtn, quitBtn;
+    AudioSource btnSound;
     // Start is called before the first frame update
     void Start()
     {
         mainMenuBtn = GameObject.Find("MainMenuBtn").GetComponent<Button>();
         playAgainBtn = GameObject.Find("PlayAgainBtn").GetComponent<Button>();
         quitBtn = GameObject.Find("QuitBtn").GetComponent<Button>();
+        btnSound = GameObject.Find("ButtonSound").GetComponent<AudioSource>();
+        DontDestroyOnLoad(btnSound);
 
         //Load the Game Scene on click
         mainMenuBtn.onClick.AddListener(() =>
         {
+            btnSound.Play();
             UnityEngine.SceneManagement.SceneManager.LoadScene("TitleScene");
         });
 
         //Load the High Score Scene on click
         playAgainBtn.onClick.AddListener(() =>
         {
+            btnSound.Play();
             UnityEngine.SceneManagement.SceneManager.LoadScene("GameScene");
         });
 
         //Exit the game on click
         quitBtn.onClick.AddListener(() =>
         {
+            btnSound.Play();
             Application.Quit();
         });
     }
--- Assets/Scripts/TitleSceneManager.cs
+++ Assets/Scripts/TitleSceneManager.cs
@@ -6,28 +6,39 @@ using UnityEngine.UI;
 public class TitleSceneManager : MonoBehaviour
 {
     Button playBtn, highScoreBtn, quitBtn;
+    AudioSource btnSound, backgroundMusic;
     // Start is called before the first frame update
     void Start()
     {
         playBtn = GameObject.Find("PlayBtn").GetComponent<Button>();
         highScoreBtn = GameObject.Find("HighScoreBtn").GetComponent<Button>();
         quitBtn = GameObject.Find("QuitBtn").GetComponent<Button>();
+        btnSound = GameObject.Find("ButtonSound").GetComponent<AudioSource>();
+        backgroundMusic = GameObject.Find("BackgroundMusic").GetComponent<AudioSource>();
+        DontDestroyOnLoad(btnSound);
+        DontDestroyOnLoad(backgroundMusic);
+
+        //Play background music if it isn't already playing
+        if (!backgroundMusic.isPlaying) backgroundMusic.Play();
 
         //Load the Game Scene on click
         playBtn.onClick.AddListener(() =>
         {
+            btnSound.Play();
             UnityEngine.SceneManagement.SceneManager.LoadScene("GameScene");
         });
 
         //Load the High Score Scene on click
         highScoreBtn.onClick.AddListener(() =>
         {
+            btnSound.Play();
             UnityEngine.SceneManagement.SceneManager.LoadScene("HighScoreScene");
         });
 
         //Exit the game on click
         quitBtn.onClick.AddListener(() =>
         {
+            btnSound.Play();
             Application.Quit();
         });
     }
